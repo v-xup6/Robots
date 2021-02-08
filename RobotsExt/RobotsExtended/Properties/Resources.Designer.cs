abd26c7aa04043170900363816a8f5f00a631529
Victor Lin <60565737+v-xup6@users.noreply.github.com>
2021-02-08 13:41:07 +0800

    Add files via upload

--- RobotsExt/RobotsExtended/Properties/Resources.Designer.cs
+++ RobotsExt/RobotsExtended/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace RobotsExtended.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Colourful {
+            get {
+                object obj = ResourceManager.GetObject("Colourful", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Define_Joints {
             get {
                 object obj = ResourceManager.GetObject("Define_Joints", resourceCulture);
